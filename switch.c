ea3ee5a3b619289a67ea5b5cae984693d015378d
pra-cpu <prashant.hosahalli2@gmail.com>
2025-11-24 21:08:21 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  int i = 1;
+  
+  switch(i){
+  	case 1: 
+      printf("ON");
+      break;
+      
+    case 2:
+      printf("OFF");
+      break;
+  }
+    
+  return 0;
+}
